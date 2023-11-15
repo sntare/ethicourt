918e5f56de933c4766065b91b0eea440e8da14cf
sntare <sntare@smu.edu>
2023-11-15 16:48:45 -0600

    update for dylan

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 let currentIndex = 0;
-const votes = {
-  for: 0,
-  neutral: 0,
-  against: 0,
-  total: 0
-};
+let questions = []; // Array to store questions
+let votes = []; // Array to store votes for each question
 
 function navigate(direction) {
-  const questions = document.getElementsByClassName('question');
-  currentIndex += direction;
-  
-  if (currentIndex < 0) {
-    currentIndex = questions.length - 1;
-  } else if (currentIndex >= questions.length) {
-    currentIndex = 0;
-  }
-  
-  updateSlider();
+    const questionsElements = document.getElementsByClassName('question');
+    currentIndex += direction;
+    console.log(direction)
+    if (currentIndex < 0) {
+        currentIndex = questionsElements.length - 1;
+    } else if (currentIndex >= questionsElements.length) {
+        currentIndex = 0;
+    }
+
+    updateSlider();
+    updatePercentages(currentIndex); // Update percentages to match the current question immediately upon navigation
 }
 
 function updateSlider() {
-  const questions = document.getElementsByClassName('ethical-questions')[0];
-  const newTransformValue = 'translateX(' + (-currentIndex * 100) + '%)';
-  questions.style.transform = newTransformValue;
+    const questionsContainer = document.getElementsByClassName('ethical-questions')[0];
+    const newTransformValue = 'translateX(' + (-currentIndex * 100) + '%)';
+    questionsContainer.style.transform = newTransformValue;
+}
+
+function postQuestion() {
+    const questionText = document.getElementById('questionInput').value;
+    if (questionText.trim()) {
+        questions.push(questionText);
+        votes.push({ for: 0, neutral: 0, against: 0, total: 0 });
+
+        const questionsContainer = document.getElementById('ethicalQuestions');
+        const newQuestionDiv = document.createElement('div');
+        newQuestionDiv.classList.add('question');
+        newQuestionDiv.textContent = questionText;
+        questionsContainer.appendChild(newQuestionDiv);
+
+        document.getElementById('questionInput').value = '';
+    }
 }
 
 function vote(opinion) {
-  votes[opinion] += 1;
-  votes.total += 1;
-  updatePercentages();
+    if (!votes[currentIndex]) {
+        votes[currentIndex] = { for: 0, neutral: 0, against: 0, total: 0 };
+    }
+
+    votes[currentIndex][opinion] += 1;
+    votes[currentIndex].total += 1;
+
+    updatePercentages(currentIndex);
 }
 
-function updatePercentages() {
-  const forPercentage = document.getElementById('forPercentage');
-  const neutralPercentage = document.getElementById('neutralPercentage');
-  const againstPercentage = document.getElementById('againstPercentage');
-  
-  forPercentage.innerText = calculatePercentage(votes.for, votes.total) + '%';
-  neutralPercentage.innerText = calculatePercentage(votes.neutral, votes.total) + '%';
-  againstPercentage.innerText = calculatePercentage(votes.against, votes.total) + '%';
+function updatePercentages(questionIndex) {
+    const forPercentage = document.getElementById('forPercentage');
+    const neutralPercentage = document.getElementById('neutralPercentage');
+    const againstPercentage = document.getElementById('againstPercentage');
+
+    forPercentage.innerText = calculatePercentage(votes[questionIndex].for, votes[questionIndex].total) + '%';
+    neutralPercentage.innerText = calculatePercentage(votes[questionIndex].neutral, votes[questionIndex].total) + '%';
+    againstPercentage.innerText = calculatePercentage(votes[questionIndex].against, votes[questionIndex].total) + '%';
 }
 
 function calculatePercentage(part, total) {
-  if (total === 0) return '0';
-  return ((part / total) * 100).toFixed(1);
+    if (total === 0) return '0';
+    return ((part / total) * 100).toFixed(1);
 }
+
+// Add other script functionalities as needed
